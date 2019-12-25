da37329391d488e12ce41c712a4144d24d307ccf
VIPHO <hey@vipho.io>
2019-12-25 17:12:46 +0300

    Add email notifier

--- api/order/services/order.js
+++ api/order/services/order.js
@@ -5,4 +5,23 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async create(data, { files } = {}) {
+    const entry = await strapi.query('order').create(data);
+
+    if (files) {
+      // automatically uploads the files based on the entry and the model
+      await this.uploadFiles(entry, files, { model: strapi.models.orders });
+      return this.findOne({ id: entry.id });
+    }
+
+    strapi.plugins['email'].services.email.send({
+      to: ['vadim.uart@gmail.com', 'hey@vipho.io'],
+      subject: 'Новый заказ!',
+      // text: '',
+      html: `Пора зайти <a href="https://api.teddybear.pp.ua/admin">админку</a>.<br>${JSON.stringify(data)}`,
+    });
+
+    return entry;
+  },
+};
